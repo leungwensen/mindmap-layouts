f6f212f072ab2c769e8b8792440c5a90cd363b3a
leungwensen <leungwensen@gmail.com>
2017-02-22 14:23:34 +0800

    first runnable edition

--- demo/src/hierarchical.js
+++ demo/src/hierarchical.js
@@ -1,4 +1,18 @@
-const TreeNode = require('../../lib/tree-node')
-const nonLayeredTidyTrees = require('../../lib/layouts/non-layered-tidy-trees')
+import TreeNode from '../../lib/tree-node'
+import {
+  Tree,
+  layout
+} from '../../lib/layouts/non-layered-tidy-trees'
+import GenerateTrees from './generate-trees'
 
-console.log(TreeNode, nonLayeredTidyTrees)
+console.log(TreeNode, Tree, layout, GenerateTrees)
+
+const gen = new GenerateTrees(50, 10, 100, 10, 100)
+
+const tree = gen.rand()
+tree.addGap(10, 10)
+// layout
+tree.layer()
+tree.normalizeX()
+const bb = tree.getBoundingBox()
+console.log(tree, bb)
